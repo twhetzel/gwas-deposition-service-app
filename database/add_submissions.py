c3070878346337fcd90b159759fa5d8599f9debf
Trish Whetzel <plwhetzel@gmail.com>
2019-06-05 16:19:27 +0100

    add more data

--- database/add_submissions.py
+++ database/add_submissions.py
@@ -50,6 +50,46 @@ session.add(User2)
 session.commit()
 print User2.name
 
+User3 = User(name="Annalisa Buniello", email="buniello@ebi.ac.uk")
+session.add(User3)
+session.commit()
+print User3.name
+
+User4 = User(name="Peggy Hall", email="Peggy.Hall@nih.gov")
+session.add(User4)
+session.commit()
+print User4.name
+
+User5 = User(name="Jackie MacArthur", email="jalm@ebi.ac.uk")
+session.add(User5)
+session.commit()
+print User5.name
+
+User6 = User(name="Lizzy Lewis", email="earl@ebi.ac.uk")
+session.add(User6)
+session.commit()
+print User6.name
+
+User7 = User(name="Maria Cerezo", email="mcerezo@ebi.ac.uk")
+session.add(User7)
+session.commit()
+print User7.name
+
+User8 = User(name="Test User", email="someone@ebi.ac.uk")
+session.add(User8)
+session.commit()
+print User8.name
+
+User9 = User(name="Aoife McMahon", email="aoifem@ebi.ac.uk")
+session.add(User9)
+session.commit()
+print User9.name
+
+User10 = User(name="Joannella Morales", email="jmorales@ebi.ac.uk")
+session.add(User10)
+session.commit()
+print User10.name
+
 
 # Submission info
 submission1 = Submission(publication_id=12334, filename='', is_valid_format=0, is_valid_data=0, user_id=1)
